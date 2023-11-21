637a46775fa4fbd9b3acfa5d95d95550a47363fc
Georgios Jason Efstathiou <j@jason-e.dev>
2023-11-21 17:03:31 +0100

    wip

--- docker/services/app/scripts/build-and-start-tests.sh
+++ docker/services/app/scripts/build-and-start-tests.sh
@@ -2,6 +2,6 @@ cd app
 
 npm run build:graphql
 
-VITE_TEST_MODE=true npm run build
+PUBLIC_TEST_SUBGRAPH_HOST=graph-node E2E_FAKE_PINATA_HOST=fake-pinata PUBLIC_TESTNET_MOCK_PROVIDER_HOST=testnet VITE_TEST_MODE=true npm run build
 
-npm run test:e2e
+PUBLIC_TEST_SUBGRAPH_HOST=graph-node E2E_FAKE_PINATA_HOST=fake-pinata PUBLIC_TESTNET_MOCK_PROVIDER_HOST=testnet npm run test:e2e
