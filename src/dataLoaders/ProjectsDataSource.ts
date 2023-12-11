b7105349cd50969a8c97ec662cceb5d9c6a05beb
Ioannis Tourkogiorgis <jtourkos@gmail.com>
2023-12-11 10:48:09 +0100

    fix: fix wrong GitHub URL regex

--- src/dataLoaders/ProjectsDataSource.ts
+++ src/dataLoaders/ProjectsDataSource.ts
@@ -43,7 +43,7 @@ export default class ProjectsDataSource {
   public async getProjectByUrl(
     url: string,
   ): Promise<ProjectModel | FakeUnclaimedProject | null> {
-    const githubUrlRegex = /^https:\/\/github\.com\/[\w-]+\/[\w-]+$/;
+    const githubUrlRegex = /^https:\/\/github\.com\/[\w-]+\/[\w.-]+$/;
 
     if (!githubUrlRegex.test(url)) {
       throw new Error('Invalid GitHub URL');
